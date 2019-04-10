9b365029dd22885778ccf0e2dca0aca1e070cecf
rozhnevay <weic8vne>
2019-04-10 21:16:01 +0300

    #17

--- src/components/Private/Chat.vue
+++ src/components/Private/Chat.vue
@@ -33,7 +33,7 @@
                     {{chat.name}}
                   </span>
                 <div class="message">
-                    {{chat.lastMessage.body}}
+                    {{chat.lastMessage().body}}
                 </div>
               </div>
 
@@ -42,168 +42,12 @@
 
             <div class="status">
               <!--<span v-if="chat.sitepower_id === activeChatId" class="badge badge-light">NOW </span>-->
-              <span v-if="chat.countUnread > 0" class="badge badge-danger">UNREAD <span  class="badge badge-light">{{chat.countUnread}}</span></span>
-              <small class="text-muted">{{chat.lastMessage.created | moment("HH:mm")}}</small>
+              <span v-if="chat.countUnread() > 0" class="badge badge-danger">UNREAD <span  class="badge badge-light">{{chat.countUnread()}}</span></span>
+              <small class="text-muted">{{chat.lastMessage().created | moment("HH:mm")}}</small>
             </div>
           </div>
 
-<!--          <div class="item">
-
-            <div class="left">
-
-              <div class="img">
-                <img src="assets/img/logo-lightning.svg" height="25px" alt="">
-                <span class="color green"></span>
-              </div>
-
-              <div class="text">
-                  <span class="title">
-                    Hosters.ru
-                  </span>
-                <div class="message">
-                  Спасибо большое за ответ…
-                </div>
-              </div>
-
-            </div>
-
-
-            <div class="status">
-              <span class="badge badge-success">ANSWERED</span>
-            </div>
-          </div>
-
-          <div class="item">
-
-            <div class="left">
-
-              <div class="img">
-                <img src="assets/img/logo-lightning.svg" height="25px" alt="">
-                <span class="color purple"></span>
-              </div>
-
-              <div class="text">
-                  <span class="title">
-                    Hosters.ru
-                  </span>
-                <div class="message">
-                  Спасибо большое за ответ…
-                </div>
-              </div>
-
-            </div>
-
-
-            <div class="status">
-              <span class="badge badge-danger">UNREAD <span class="badge badge-light">5</span></span>
-            </div>
-          </div>
-
-          <div class="item">
-
-            <div class="left">
-
-              <div class="img">
-                <img src="assets/img/logo-lightning.svg" height="25px" alt="">
-                <span class="color purple"></span>
-              </div>
-
-              <div class="text">
-                  <span class="title">
-                    Hosters.ru
-                  </span>
-                <div class="message">
-                  Спасибо большое за ответ…
-                </div>
-              </div>
-
-            </div>
-
-
-            <div class="status">
-              <span class="badge badge-danger">UNREAD <span class="badge badge-light">5</span></span>
-            </div>
-          </div>
-
-          <div class="item">
-
-            <div class="left">
-
-              <div class="img">
-                <img src="assets/img/logo-lightning.svg" height="25px" alt="">
-                <span class="color purple"></span>
-              </div>
-
-              <div class="text">
-                  <span class="title">
-                    Hosters.ru
-                  </span>
-                <div class="message">
-                  Спасибо большое за ответ…
-                </div>
-              </div>
-
-            </div>
-
-
-            <div class="status">
-              <span class="badge badge-danger">UNREAD <span class="badge badge-light">5</span></span>
-            </div>
-          </div>
-
-          <div class="item">
-
-            <div class="left">
-
-              <div class="img">
-                <img src="assets/img/logo-lightning.svg" height="25px" alt="">
-                <span class="color purple"></span>
-              </div>
-
-              <div class="text">
-                  <span class="title">
-                    Hosters.ru
-                  </span>
-                <div class="message">
-                  Спасибо большое за ответ…
-                </div>
-              </div>
-
-            </div>
-
-
-            <div class="status">
-              <span class="badge badge-danger">UNREAD <span class="badge badge-light">5</span></span>
-            </div>
-          </div>
-
-          <div class="item">
-
-            <div class="left">
-
-              <div class="img">
-                <img src="assets/img/logo-lightning.svg" height="25px" alt="">
-                <span class="color purple"></span>
-              </div>
-
-              <div class="text">
-                  <span class="title">
-                    Hosters.ru
-                  </span>
-                <div class="message">
-                  Спасибо большое за ответ…
-                </div>
-              </div>
-
-            </div>
-
-
-            <div class="status">
-              <span class="badge badge-danger">UNREAD <span class="badge badge-light">5</span></span>
-            </div>
-          </div>-->
-
-        </div>
+         </div>
       </div>
     <chat-body-component :id="activeChatId"></chat-body-component>
     <chat-info-component :id="activeChatId"></chat-info-component>
@@ -219,6 +63,7 @@
   import * as jquery from 'jquery'
   import Vue from 'vue';
   Vue.use(require('vue-moment'));
+  import moment from 'moment';
   export default {
     name: 'Chat',
     data () {
@@ -228,7 +73,35 @@
     },
     mounted() {
       axios.get("/api/chats").then((res) => {
-        this.$store.commit('initChats', res.data);
+        let chats = res.data;
+
+        chats.map(item => {
+          item.lastMessage = () => {
+            if (item.messages && item.messages.length) {
+              return item.messages[item.messages.length - 1];
+            }
+            return {};
+          };
+          item.countUnread = () => {
+            console.log("countUnread");
+            if (item.messages && item.messages.length) {
+              let unread = item.messages.filter(u => moment(u.created) > moment(item.lastOpenDt))
+              if (unread && unread.length > 0) {
+                return unread.length;
+              }
+            }
+            return 0;
+          }
+        });
+        chats.sort((a,b)=> {
+          let alastMC = a.lastMessage().created;
+          let blastMC = b.lastMessage().created;
+          if (moment(alastMC) < moment(blastMC)) return -1
+          else if (moment(alastMC) > moment(blastMC)) return 1
+          else return 0
+        });
+        console.log(chats);
+        this.$store.commit('initChats', chats);
       }).catch((err) => {
         console.log("Error in getting chats: ", err.message)
       })
